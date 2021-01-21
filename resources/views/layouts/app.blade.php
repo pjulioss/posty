d810b63932d405e89799bc16d9fd65c18c5d1360
Pedro Julio <soaresjuliopedro@gmail.com>
2021-01-21 18:31:22 -0300

    Instalado e configurado Tailwind

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Posty</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-4">
+        <ul class="flex items-center">
+            <li><a href=""class="p-3">Home</a></li>
+            <li><a href=""class="p-3">Painel</a></li>
+            <li><a href=""class="p-3">Post</a></li>
+        </ul>
+        <ul class="flex items-center">
+            <li><a href=""class="p-3">Pedro Julio</a></li>
+            <li><a href=""class="p-3">Entrar</a></li>
+            <li><a href=""class="p-3">Registro</a></li>
+            <li><a href=""class="p-3">Sair</a></li>
+        </ul>
+    </nav>
+    @yield('content')
+</body>
+
+</html>
